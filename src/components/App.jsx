9ab06ec0ce3f061bd70930df0d48fbf8b3e9ce93
wassaybajwa <wassaybajwa@hotmail.com>
2021-08-05 02:10:06 -0400

    React Forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,16 @@ import React, { useState } from "react";
 
 function App() {
 
-  const [headingText, setHeadingText] = useState("Hello")
+  const [headingText, setHeadingText] = useState("")
   const [isMouseOver, setMouseOver] = useState(false)
+
+  const [name, setName] = useState("")
+
   function handleClick() {
 
-    setHeadingText("Submitted");
+    setHeadingText(name);
+
+    event.preventDefault();
   }
 
   function handleMouseOver() {
@@ -19,18 +24,25 @@ function App() {
 
     setMouseOver(false)
   }
+
+  function handleChange(){
+
+    setName(event.target.value)
+  }
   
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button style={{ backgroundColor: isMouseOver ? "black" : "white"  }} 
-      onClick={handleClick}
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      >
+      <h1>Hello! {headingText} {name}</h1>
+      <form onSubmit={handleClick}>
+        <input onChange={handleChange} value={name} type="text" placeholder="What's your name?" />
+        <button style={{ backgroundColor: isMouseOver ? "black" : "white"  }} 
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        type="submit">
         
         Submit</button>
+      </form>
     </div>
   );
 }
